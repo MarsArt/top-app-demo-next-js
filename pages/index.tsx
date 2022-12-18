8328d1dc5b8500f4e4da71bcbfc4344cae6ef911
Maksim Artemenko <maxim.a@overdose.digital>
2022-12-18 13:44:04 +0300

    add first component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image';
+import React from 'react';
+import { Htag } from '../components';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home(): JSX.Element {
     return (
         <div>
             Hello with fonts
+            <Htag tag='h1'>Текст</Htag>
         </div>
     );
 }
