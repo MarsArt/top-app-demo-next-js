0c7733d147e540237c182fbeb2dc143f2952ab42
Maksim Artemenko <maxim.a@overdose.digital>
2022-12-18 22:49:02 +0300

    add tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
     return (
@@ -8,6 +8,13 @@ export default function Home(): JSX.Element {
             <Htag tag='h1'>Текст</Htag>
             <Button apperance='primary' arrow='right'>Кнопка</Button>
             <Button apperance='ghost' arrow='down'>Кнопка</Button>
+            <P size='l'>Большой</P>
+            <P>Средний</P>
+            <P size='s'>Маленький</P>
+            <Tag size='s'>GHOST</Tag>
+            <Tag size='m' color='red'>Red</Tag>
+            <Tag size='s' color='green'>GREEN</Tag>
+            <Tag color='primary'>PRIMARY</Tag>
         </div>
     );
 }
