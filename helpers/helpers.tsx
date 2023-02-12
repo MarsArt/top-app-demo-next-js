7f754de5c2be5d526d8b57dfbfc1167ffb86e893
Maksim Artemenko <maxim.a@overdose.digital>
2023-02-12 18:11:09 +0300

    add menu dynamic

---
+++ helpers/helpers.tsx
@@ -0,0 +1,15 @@
+
+import CoursesIcon from './icons/courses.svg';
+import BooksIcon from './icons/books.svg';
+import ServisesIcon from './icons/services.svg';
+import ProductsIcon from './icons/products.svg';
+import { TopLevelCategory } from '../interfaces/page.interface';
+import { FirstLevelMenuItem } from '../interfaces/menu.interface';
+
+
+export const firstLevelMenu: FirstLevelMenuItem[] = [
+    { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+    { route: 'services', name: 'Сервисы', icon: <ServisesIcon />, id: TopLevelCategory.Services },
+    { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+    { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+];
