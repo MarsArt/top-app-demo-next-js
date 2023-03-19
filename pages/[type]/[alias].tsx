c9cb8be20c8c2a6967715d674aedd599bd36b3fa
Maksim Artemenko <maxim.a@overdose.digital>
2023-03-19 09:21:57 +0300

    add card component

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -7,20 +7,22 @@ import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface'
 import { ParsedUrlQuery } from 'querystring';
 import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
+import { TopPageComponent } from '../../page-components';
 
 
-function Course({ menu, page, products }: CourseProps): JSX.Element {
-    return (
-        <>
-            {products && products.length}
-        </>
+function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
 
-    );
+
+
+    return <TopPageComponent
+        firstCategory={firstCategory}
+        page={page}
+        products={products}
+    />;
 }
 
 
-
-export default withLayout(Course);
+export default withLayout(TopPage);
 
 export const getStaticPaths: GetStaticPaths = async () => {
     let paths: string[] = [];
@@ -40,7 +42,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 
-export const getStaticProps: GetStaticProps<CourseProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
+export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
     if (!params) {
         return {
             notFound: true
@@ -55,7 +57,7 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({ params }: Ge
         };
     }
 
-    try{
+    try {
 
         const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
             firstCategory: fisstCategoryItem.id
@@ -89,7 +91,7 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({ params }: Ge
     }
 };
 
-interface CourseProps extends Record<string, unknown> {
+interface TopPageProps extends Record<string, unknown> {
     menu: MenuItem[];
     firstCategory: TopLevelCategory;
     page: TopPageModel;
