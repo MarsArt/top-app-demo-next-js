cd49824592cd383ff1e5bfb428b3c856a9b1ce12
Maksim Artemenko <maxim.a@overdose.digital>
2023-07-16 17:34:50 +0300

    add review form

---
+++ components/Review/Review.tsx
@@ -0,0 +1,32 @@
+import { ReviewProps } from './Review.props';
+import styles from './Review.module.css';
+import UserIcon from './User.svg';
+import cn from 'classnames';
+import { format } from 'date-fns';
+import { ru } from 'date-fns/locale';
+import { Rating } from '../Rating/Rating';
+
+export const Review = ({ review, children, className, ...props }: ReviewProps): JSX.Element => {
+
+    const { name, title, description, createdAt, rating } = review;
+    return (
+        <div className={cn(styles.review, className,)}
+            {...props}
+        >
+            <UserIcon className={styles.user} />
+            <div>
+                <span className={styles.name}>{name}:</span>&nbsp;&nbsp;
+                <span className={styles.title}>{title}</span>
+            </div>
+            <div className={styles.data}>
+                {format(new Date(createdAt), 'dd MMMM yyyy', { locale: ru })}
+            </div>
+            <div className={styles.rating}>
+                <Rating rating={rating} />
+            </div>
+            <div className={styles.description}>
+                {description}
+            </div>
+        </div>
+    );
+};
